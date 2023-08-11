a8fdfb2ed438d2c59f1a21a0a3cae99e8989ac49
BisherRefai963 <201639133@fa-hists.edu.eg>
2023-08-11 16:37:25 -0700

    bichr_calculation 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+def add(k, l):
+    """My addition function
+
+    Args:
+        k: first integer
+        l: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (k + l)
+
+
+def sub(k, l):
+    """My subtraction function
+
+    Args:
+        k: first integer
+        l: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (k - l)
+
+
+def mul(k, l):
+    """My multiplication function
+
+    Args:
+        k: first integer
+        l: second integer
+
+    Returns:
+        The return value. k * l
+    """
+    return (k * l)
+
+
+def div(k, l):
+    """My division function
+
+    Args:
+        k: first integer
+        l: second integer
+
+    Returns:
+        The return value. k / l
+    """
+    return int(k /l)
+
