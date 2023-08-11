e34746c4963dab6e2c8edba82298bf93e3b62a5b
BisherRefai963 <201639133@fa-hists.edu.eg>
2023-08-11 16:25:14 -0700

    Bichr_Rifai_add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add(k, l):
+    """My addition function
+
+    Args:
+        k: first integer
+        l: second integer
+
+    Returns:
+        The return value. k + l
+    """
+    return (k + l)
+
